6950756e29a3a19e25756435a24cf86e0a44f8a7
Lorenzo Sapora <lorenzo.sapora@gmail.com>
2021-09-24 13:20:55 +0100

    Create nova component

---
+++ src/NovaActionButton.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Lorenzosananopora\NovaActionButton;
+
+use Laravel\Nova\Fields\Field;
+
+class NovaActionButton extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'nova-action-button';
+}
